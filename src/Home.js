89445425016c2a6f2b4f82d5f3ac43bd737fea98
SofijaSutton <srlsutton@gmail.com>
2021-03-14 17:14:48 -0400

    created Home component

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as THREE from 'three';
+import {MTLLoader, OBJLoader} from 'three-obj-mtl-loader';
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
+
+/**
+ * COMPONENT
+ */
+class Home extends React.Component {
+}
+
+export default Home
