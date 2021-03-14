e4281746392060bee5569488763f6668c4ea6881
SofijaSutton <srlsutton@gmail.com>
2021-03-14 17:16:41 -0400

    add componentDidMount function to Home with variable settings

--- src/Home.js
+++ src/Home.js
@@ -7,6 +7,38 @@ import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
  * COMPONENT
  */
 class Home extends React.Component {
+    componentDidMount() {
+        const width = this.mount.clientWidth
+        const height = this.mount.clientHeight
+        this.scene = new THREE.Scene()
+
+        //Add Renderer
+        this.renderer = new THREE.WebGLRenderer({antialias: true})
+        this.renderer.setClearColor('#ffffff')
+        this.renderer.setSize(width, height)
+        this.mount.appendChild(this.renderer.domElement)
+
+        //add Camera
+        this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000)
+        this.camera.position.z = 8
+        this.camera.position.y = 5
+
+        //LIGHTS
+        var lights = []
+        lights[0] = new THREE.PointLight(0x304ffe, 1, 0)
+        lights[1] = new THREE.PointLight(0xffffff, 1, 0)
+        lights[2] = new THREE.PointLight(0xffffff, 1, 0)
+        lights[0].position.set(0, 200, 0)
+        lights[1].position.set(100, 200, 100)
+        lights[2].position.set(-100, -200, -100)
+        this.scene.add(lights[0])
+        this.scene.add(lights[1])
+        this.scene.add(lights[2])
+
+        this.addModels()
+        this.renderScene()
+        this.start()
+    }
 }
 
 export default Home
